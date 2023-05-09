6be5e906b6272979e7857475139be0982ff55fd8
saif <sirsaif99@gmail.com>
2023-05-09 14:03:15 +0300

    pain: memray 1st date

---
+++ spells/fluent_python/memray_1st_date.py
@@ -0,0 +1,41 @@
+# def foo(n):
+#     x = []
+#     for _ in range(n):
+#         x.append(None)
+#     return x
+#
+#
+# foo(1_000_000)
+
+def a(n):
+    return [b(n), h(n)]
+
+def b(n):
+    return c(n)
+
+def c(n):
+    missing(n)
+    return d(n)
+
+def missing(n):
+    return "a" * n
+
+def d(n):
+    return [e(n), f(n), "a" * (n // 2)]
+
+def e(n):
+    return "a" * n
+
+def f(n):
+    return g(n)
+
+def g(n):
+    return "a" * n * 2
+
+def h(n):
+    return i(n)
+
+def i(n):
+    return "a" * n
+
+a(100000)
